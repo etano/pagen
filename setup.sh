4b9848c85d3d4fb32598bd105ad973510ec350a1
Ethan Brown <ethan.w.brown@gmail.com>
2014-11-24 14:23:30 +0100

    added ewald

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd davidSquarer && ./machconfig && make && cd -
-cd ilkkaSquarer && make && cd -
-cd exact && f2py -c -m coulcc coulcc36-f90.f90 && cd -
+cd davidSquarer && ./machconfig && make && cd ..
+cd ilkkaSquarer && make && cd ..
+cd exact && f2py -c -m coulcc coulcc36-f90.f90 && cd ..
+cd ewald && [[ -d builds ]] || mkdir builds && cd build && cmake .. && make && cd .. && cd ..
