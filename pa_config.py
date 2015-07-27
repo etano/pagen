9863b1a3eadfa412fc8a4f86079060e987d4c314
Ethan Brown <ethan.w.brown@gmail.com>
2015-07-27 21:38:03 +0200

    updated pa_config.py to have LOGLIN

--- pa_config.py
+++ pa_config.py
@@ -21,7 +21,8 @@ potential['function'] = lambda Z1,Z2,r: Z1*Z2/r
 potential['r_min'] = 0.0001 # first grid point
 potential['r_max'] = 100. # last grid point
 potential['n_grid'] = 1000 # number grid points
-potential['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka only!))
+potential['grid_type'] = "OPTIMIZED" # grid type (LOG, LINEAR, OPTIMIZED (Ilkka only!), LOGLIN (David only!))
+potential['r_paste'] = L/4. # where the LOG and LINEAR grids would be pasted for LOGLIN
 
 # Squarer
 squarer = {}
@@ -30,7 +31,8 @@ squarer['tau'] = tau # desired timestep of PIMC simulation
 squarer['n_d'] = D # dimension
 squarer['r_max'] = 100.0 # maximum distance on grid
 squarer['n_grid'] = 100 # number of grid points
-squarer['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka only!))
+squarer['grid_type'] = "OPTIMIZED" # grid type (LOG, LINEAR, OPTIMIZED (Ilkka only!), LOGLIN (David only!))
+squarer['r_paste'] = L/4. # where the LOG and LINEAR grids would be pasted for LOGLIN
 squarer['n_square'] = 33 # total number of squarings to reach lowest temperature
 squarer['n_order'] = -1 # order of off-diagonal PA fit: -1 = no fit (direct spline, Ilkka only!), 0 = only diagonal, 1-3 = fit off-diagonal to 1-3 order
 squarer['n_temp'] = 1 # number of temperatures for which to calculate the pair action (David only!)
@@ -46,7 +48,8 @@ breakup['r_max'] = sqrt(breakup['n_d'])*breakup['L']/2. # last grid point
 breakup['r_cut'] = breakup['L']/2. # r cutoff for ewald
 breakup['k_cut'] = 14./(L/2.) # k cutoff for ewald
 breakup['n_grid'] = 1000 # number of grid points
-breakup['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka only!))
+breakup['grid_type'] = "OPTIMIZED" # grid type (LOG, LINEAR, OPTIMIZED (Ilkka only!), LOGLIN (David only!))
+breakup['r_paste'] = L/4. # where the LOG and LINEAR grids would be pasted for LOGLIN
 breakup['n_knots'] = 10 # number of knots in spline (probably fine)
 breakup['n_images'] = 10 # Naive check
 
